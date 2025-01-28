55e01a0b02f262cdc25ae19a941a4e4ae85b99ea
szczekulskij <szczekulskij@gmail.com>
2025-01-28 15:42:02 -0800

    add the script changes

--- ad_hoc_cri_lab_blender/README.md
+++ ad_hoc_cri_lab_blender/README.md
@@ -13,3 +13,9 @@ Furthermore, we store the rendered 2D images in the `objaverse-rendering/objaver
 export PATH=$PATH:/path/to/blender/executable
 ```
 3. Install python dependencies via `python3 -m pip install -r requirements_blender.txt`
+
+
+# Requirements for running script
+* Look into `ad_hoc_cri_lab_blender/render_blender.sh`
+* Set the variables within the shell script above and run it
+* (Optional) Change `METAL` to `CUDA` in `blender_script.py` (eg. line of code `.preferences.compute_device_type = "METAL" # or "CUDA" or "OPENCL"`)
--- ad_hoc_cri_lab_blender/render_blender.sh
+++ ad_hoc_cri_lab_blender/render_blender.sh
@@ -1,54 +1,31 @@
 #!/bin/bash
 
-: '
-Robust - itll only download 3d objects that havent been rendered yet
-To specify path for images/3d objects download look into blender_script.py
-
-Had to be setup this way in a shell script, since any script run from blender HAS to use blenders python 
-(and therefore cant have any dependencies installed such as objaverse package)
-'
-
 alias blender=/Applications/Blender.app/Contents/MacOS/Blender # pointer to blender executable
-# 1. Set up variables
-# num_objects=2000
-num_objects=200
-num_images=8 # per object
-# img_resolution=512
+
+# 1. Set following variables
+filename=""  # path to 3d object file
+num_images=10
 img_resolution=256
-# img_resolution=128
+image_path= "~/Desktop/blender_output"
 
-# 2. Download 3d objects
-# python3 -m pip install -r requirements_blender.txt # can be commented out if already installed
-python3 objaverse_download.py --num_objects $num_objects # this will download blender files and save filenames in filenames.txt file (it'll delete the file if it existed before running this script)
-filenames=$(cat filenames.txt)
+# 2. If image path does not exist, create it
+if [ ! -d "$image_path" ]; then
+    mkdir -p $image_path
+fi
 
-# 2.1 Some robustness
-# 2.1.1 Check if filenames.txt is empty
-if [ ! -s filenames.txt ]
-then
-    echo "Error: filenames.txt is empty. Please ensure it contains filenames."
+# 3. Check all params have been set and throw error if not
+if [ -z "$filename" ]; then
+    echo "Error: Please set the filename variable to the path of the 3d object file"
     exit 1
 fi
 
-# 2.1.2 Check if filenames variable is set
-if [ -z "$filenames" ]
-then
-    echo "Error: No filenames found. Please check filenames.txt."
-    exit 1
-fi
+echo "Processing file for object: $filename"
+blender -b -P blender_script.py -- \
+    --nr_images $num_images \
+    --engine CYCLES \
+    --scale 0.8 \
+    --camera_dist 1.2  \
+    --img_resolution $img_resolution \
+    --filename $filename 
 
-#3. Run blender script for each object
-for filename in $filenames; 
-do
-    echo "Processing file for object: $filename"
-    blender -b -P blender_script.py -- \
-        --nr_images $num_images \
-        --engine CYCLES \
-        --scale 0.8 \
-        --camera_dist 1.2  \
-        --img_resolution $img_resolution \
-        --filename $filename 
-done
-
-# #4. Delete 3d objects (so they dont take up space)
-python3 objaverse_delete.py --filenames $filenames
+echo "Done rendering images for object: $filename"
